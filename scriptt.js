857a8d3cf157f420750fa7ed6f4a79c9481fc0a1
elaizavillegas <31671266+elaizavillegas@users.noreply.github.com>
2017-12-12 12:34:08 -0800

    Create scriptt.js

---
+++ scriptt.js
@@ -0,0 +1,14 @@
+//I used the Joshua Tree activity as a reference and also collaborating with Alex to find out how to inplement the javascript into my html//
+//file:///C:/Users/elaiza_villegas/Downloads/joshuatree_getelementbyid/JoshuaTree_getElementById/index.html
+
+function toggle(elem){
+	elem.classList.toggle("square2");
+}
+
+function togle(elem){
+	elem.classList.toggle("square3");
+}
+
+function playSound(e){
+	document.getElementById("yellow").play();
+}
