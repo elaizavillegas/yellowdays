05a55b391177c2097731681208d1e8d0ec786c35
elaizavillegas <31671266+elaizavillegas@users.noreply.github.com>
2017-12-12 12:51:12 -0800

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,3 +1,4 @@
+//https://www.w3schools.com/js/js_whereto.asp -->how identify functions in html//
 //I used the Joshua Tree activity as a reference and also collaborating with Alex to find out how to inplement the javascript into my html//
 //file:///C:/Users/elaiza_villegas/Downloads/joshuatree_getelementbyid/JoshuaTree_getElementById/index.html
 
@@ -9,6 +10,6 @@ function togle(elem){
 	elem.classList.toggle("square3");
 }
 
-function playSound(e){
+function playSound(elem){
 	document.getElementById("yellow").play();
 }
